ea713ddd1062d837f032e9824dd334a731605f9b
Patrik Pavan <patrik.pavan20@gmail.com>
2025-05-06 15:24:02 +0200

    add idd_data function

--- dataset/data.py
+++ dataset/data.py
@@ -5,8 +5,10 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset, random_split
 
-from typing import List, Tuple, Optional, DataSet
+from typing import List, Tuple, Optional, Dict
 
+import numpy as np
+import random
 
 class Dataset:
 
@@ -54,4 +56,51 @@ class Dataset:
                                 shuffle=True, num_workers=2)
         return dataloader
     
+    def idd_data(self, dataset: Dataset, num_clients: int) -> Dict[int, List[int]]:
+
+        """
+        Create a list of indices for each client
+        """
+
+        indices_clients = {i:[] for i in range(num_clients)}
+        indices = list(range(len(dataset)))
+        data_per_client = len(dataset) // num_clients
+
+        np.random.shuffle(indices)
+
+        for i in range(num_clients):
+
+            start_idx = i * data_per_client
+            end_idx = (i + 1) * data_per_client
+            client_indices = indices[start_idx:end_idx]
+
+            indices_clients[i] = client_indices
+
+        if end_idx < len(dataset):
+
+            remaining_indices = indices[end_idx:]
+
+            for i in range(len(remaining_indices)):
+
+                indices_clients[random.randrange(num_clients)].append(remaining_indices[i])
+
+        return indices_clients
+
+
+
     
+    def non_idd_data(self, dataset: Dataset, num_clients: int) -> List[int]:
+        """
+        Create a list of dataloaders for each client
+        """
+        data_per_client = len(dataset) // num_clients
+        data_loaders = []
+        
+        for i in range(num_clients):
+            start = i * data_per_client
+            end = (i + 1) * data_per_client if i != num_clients - 1 else len(dataset)
+            indices = list(range(start, end))
+            data_loader = self.get_dataloader(dataset, indices)
+            data_loaders.append(data_loader)
+        
+        return data_loaders
