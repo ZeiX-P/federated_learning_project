162a09dba0187d1b67d66d3285a8a8a3065dd973
Patrik Pavan <patrik.pavan20@gmail.com>
2025-05-13 18:36:02 +0200

    a

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
                           optimizer_class=torch.optim.SGD,
                           loss_function=nn.CrossEntropyLoss(),
                           scheduler_class=torch.optim.lr_scheduler.CosineAnnealingLR,
-                          epochs=10,
+                          epochs=25,
                           optimizer_params={"momentum": 0.9, "weight_decay": 5e-4},
                           scheduler_params={"T_max": 20})
     
