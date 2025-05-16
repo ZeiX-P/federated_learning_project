0333743615c48442a1e537b222aca228c62778a6
Patrik Pavan <patrik.pavan20@gmail.com>
2025-05-16 21:47:19 +0200

    fix

--- models/federated_learning.py
+++ models/federated_learning.py
@@ -863,7 +863,7 @@ class FederatedLearning:
                 train_loader = DataLoader(data_client_train_set, batch_size=self.config.batch_size, shuffle=True)
                 
                 fisher = self.compute_fisher_diag(self.local_models[client], train_loader,self.config.loss_function)
-                mask = self.create_fisher_mask(fisher,0.5)
+                mask = self.create_fisher_mask(fisher, self.local_models[client], 0.5)
                 dict_local_mask[client] = mask
 
             global_mask = self.aggregate_sensitivity_scores(dict_local_mask, 0.5)
