ed331f01da6e3c1fa5cf53e8fa847e343f49663b
Patrik Pavan <patrik.pavan20@gmail.com>
2025-05-25 20:05:31 +0200

    a

--- utils/models_utils.py
+++ utils/models_utils.py
@@ -5,7 +5,7 @@ def apply_model_diff(self, global_model, update_dict):
         if name in update_dict and param.requires_grad:
             param.data += update_dict[name].to(param.device)
 
-def compute_model_diff(self, model_before, model_after):
+def compute_model_diff(model_before, model_after):
     diff = {}
     for (name, p_before), (_, p_after) in zip(model_before.named_parameters(), model_after.named_parameters()):
         if p_before.requires_grad:
