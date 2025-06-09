6725e8a7ea30041d73cbe38eedf8460795e151ad
Patrik Pavan <patrik.pavan20@gmail.com>
2025-06-09 22:07:34 +0200

    a

---
+++ bin/run_federated_averaging.py
@@ -0,0 +1,63 @@
+
+from config import Configuration
+from dataset.data import Dataset
+from models.federated_learning import FederatedLearning
+import timm
+import torch
+import torch.nn as nn
+import logging
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+    data = Dataset()
+    '''
+    config = Configuration(
+                          batch_size=32,
+                          learning_rate=0.01,
+                          momentum=0.9,
+                          weight_decay=5e-4,
+                          dataset="CIFAR100",
+                          optimizer=torch.optim.SGD,
+                          loss_function=nn.CrossEntropyLoss())
+    
+    '''
+    
+    dino = timm.create_model('vit_small_patch16_224.dino', pretrained=True)
+    for param in dino.parameters():
+        param.requires_grad = False
+    dino.head = nn.Linear(384, 100)
+
+    for param in dino.head.parameters():
+        param.requires_grad = True
+
+    config1 = Configuration(
+                          model = dino,
+                          training_name="federated_learning_model_editing_talos",
+                          batch_size=64,
+                          learning_rate=0.01,
+                          momentum=0.9,
+                          weight_decay=5e-4,
+                          dataset="CIFAR100",
+                          optimizer_class=torch.optim.SGD,
+                          loss_function=nn.CrossEntropyLoss(),
+                          scheduler_class=torch.optim.lr_scheduler.CosineAnnealingLR,
+                          epochs=10,
+                          optimizer_params={"momentum": 0.9, "weight_decay": 5e-4},
+                          scheduler_params={"T_max": 20},
+                          project_name="federated_learning_model_editing_talos")
+
+    federated_learning = FederatedLearning(global_model=dino,data=data, num_clients=100, 
+                                           aggregation_method="FedAvg", num_rounds=30,
+                                            epochs_per_round=4, distribution_type="non-iid-dirichlet",
+                                            client_fraction=0.1,config=config1)
+    
+    print("Starting Federated Learning process...")
+    federated_learning.run_federated_learning()
+
+    print("Federated Learning process completed.")
